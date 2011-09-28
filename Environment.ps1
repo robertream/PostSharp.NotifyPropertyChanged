cff7cdc0b8941959d34c46ccfaedc3be90a5684c
Robert Ream <robertream@gmail.com>
2011-09-28 00:25:32 -0700

    looking for psake in wrong folder

--- Environment.ps1
+++ Environment.ps1
@@ -1,5 +1,5 @@
 if(-not(Get-Module -name psake))
-{	
+{
 	$psake_path = Get-ChildItem .\packages\psake.* | Sort-Object Name | Select-Object -First 1
-	Import-Module $psake_path\psake.psm1
+	Import-Module $psake_path\tools\psake.psm1
 }
