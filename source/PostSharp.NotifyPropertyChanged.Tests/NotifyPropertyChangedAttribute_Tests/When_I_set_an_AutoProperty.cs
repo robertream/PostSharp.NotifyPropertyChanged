51d56e77dfe039269f4e3927f65adf7086994f09
MGALFAPAIR <MGALFAPAIR@MGALFAPAIR5>
2013-06-05 10:34:10 -0700

    NotifyObservedReferenceChanged can observe on interfaces now

--- source/PostSharp.NotifyPropertyChanged.Tests/NotifyPropertyChangedAttribute_Tests/When_I_set_an_AutoProperty.cs
+++ source/PostSharp.NotifyPropertyChanged.Tests/NotifyPropertyChangedAttribute_Tests/When_I_set_an_AutoProperty.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections.Generic;
-using System.ComponentModel;
+﻿using System.ComponentModel;
 using NUnit.Framework;
 
 // ReSharper disable InconsistentNaming
@@ -10,9 +9,8 @@ namespace PostSharp.NotifyPropertyChanged.Tests
         [Test]
         public static void When_I_set_an_AutoProperty()
         {
-            var propertiesThatChanged = new List<string>();
             var notify = new NotifyAutoProperty();
-            notify.PropertyChanged += (@object, @event) => propertiesThatChanged.Add(@event.PropertyName);
+            var propertiesThatChanged = notify.ObservePropertyChanges();
 
             notify.AutoProperty = "Test";
 
---
+++ source/PostSharp.NotifyPropertyChanged.Tests/NotifyPropertyChangedExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+
+namespace PostSharp.NotifyPropertyChanged.Tests
+{
+    public static class NotifyPropertyChangedExtensions
+    {
+        public static List<string> ObservePropertyChanges(this object @this)
+        {
+            var propertyChanges = new List<string>();
+            var viewModel = @this as INotifyPropertyChanged;
+            if (viewModel != null)
+                viewModel.PropertyChanged += (sender, args) => propertyChanges.Add(args.PropertyName);
+            return propertyChanges;
+        }
+    }
+}
